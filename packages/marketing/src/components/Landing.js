f38fea1cc1dc6f8b3da7986b94dc532c96465221
Jeffrey Bowden <jbowden@narrativescience.com>
2021-08-31 11:54:06 -0500

    change text back

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Pageroni
+              Home Page
             </Typography>
             <Typography
               variant="h5"
