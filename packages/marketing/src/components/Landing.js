fd922918b44dd05c16d8528e64c857cd855fdff8
Jeffrey Bowden <jbowden@narrativescience.com>
2021-08-31 11:49:17 -0500

    text update

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Pageroni
             </Typography>
             <Typography
               variant="h5"
